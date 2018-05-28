f80c0d8f2a2745c8402735e2002c78d307616efe
Arkadiusz Pituła <apitula@corelogic.pl>
2018-05-28 11:57:25 +0200

    Initial Commit

---
+++ Logger/Logger.h
@@ -0,0 +1,19 @@
+//
+//  Logger.h
+//  Logger
+//
+//  Created by Arkadiusz Pituła on 28.05.2018.
+//  Copyright © 2018 Arkadiusz Pituła. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Logger.
+FOUNDATION_EXPORT double LoggerVersionNumber;
+
+//! Project version string for Logger.
+FOUNDATION_EXPORT const unsigned char LoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Logger/PublicHeader.h>
+
+
